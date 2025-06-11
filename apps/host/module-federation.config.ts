8b30920f8d5a50831751dc8e827704843051975a
Leandro Fonseca <leandromkt@outlook.com>
2025-06-10 21:12:43 -0300

    feat: add URL of remotes in all apps

--- apps/host/module-federation.config.ts
+++ apps/host/module-federation.config.ts
@@ -4,8 +4,11 @@ export const mfConfig: ModuleFederationPluginOptions = {
   name: "host",
   filename: "remoteEntry.js",
   remotes: {
-    home: "home@http://localhost:3001/remoteEntry.js",
-    settings: "settings@http://localhost:3002/remoteEntry.js"
+    // home: "home@http://localhost:3001/remoteEntry.js",
+    // settings: "settings@http://localhost:3002/remoteEntry.js"
+    
+    home: "home@https://leandro-fonseca-1302-home-technicaltaskvalor-lean-7c663af09-ze.zephyrcloud.app/remoteEntry.js",
+    settings: "settings@https://leandro-fonseca-1304-settings-technicaltaskvalor--9ebf49654-ze.zephyrcloud.app/remoteEntry.js"
   },
   exposes: {
     "./RemoteEntry": "./src/App.tsx"
--- apps/settings/module-federation.config.ts
+++ apps/settings/module-federation.config.ts
@@ -7,7 +7,9 @@ export const mfConfig: ModuleFederationPluginOptions = {
     "./RemoteEntry": "./src/RemoteEntry.tsx"
   },
   remotes: {
-    host: "host@http://localhost:3000/remoteEntry.js"
+    // host: "host@http://localhost:3000/remoteEntry.js"
+    
+    host: "host@https://leandro-fonseca-1303-host-technicaltaskvalor-lean-b4ff7e770-ze.zephyrcloud.app/remoteEntry.js"
   },
   shared: ["react", "react-dom", "react-router"]
 };
